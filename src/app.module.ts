17383abd31f811e2067387a3b13d0a29af03f38a
i-m-aditya <prblmslvr.aditya@gmail.com>
2022-09-15 16:08:28 +0530

    remove unnecessary folders and code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as dotenv from "dotenv";
-import { CatsModule } from './cats/cats.module';
 import { OptionSellerPositionModule } from './positions/OpionSellers/optionSellerPositions.module';
 import { OptionBuyerPositionModule } from './positions/OptionBuyers/optionBuyerPositions.module';
 import { RecentTxsModule } from './positions/RecentTxs/recentTxs.module';
@@ -16,7 +15,6 @@ dotenv.config()
     MongooseModule.forRoot(process.env.MONGO_URI, {
       dbName: "test",
     }),
-    CatsModule,
     OptionSellerPositionModule,
     OptionBuyerPositionModule,
     RecentTxsModule
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("Ehllo");
   app.enableCors()
   await app.listen(3500);
 }
